a96538730a22d2013a4622a7a45c0146001baa45
Carlos Martins <guttomarttins@gmail.com>
2018-07-16 19:37:05 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/cateogia.service';
 
 @IonicPage()
 @Component({
@@ -8,10 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
+    this.categoriaService.findAll()
+      .subscribe(response => {
+         console.log(response);
+      },
+      error => {
+        console.log(error);
+      });
+
     console.log('ionViewDidLoad CategoriasPage');
   }
 
