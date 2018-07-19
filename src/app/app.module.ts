191862d76f3f00716c3014f9bc187a60002f574c
Carlos Martins <guttomarttins@gmail.com>
2018-07-19 20:21:56 -0300

    base do app finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/cateogia.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -28,7 +32,11 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    ErrorInterceptorProvider
+    AuthInterceptorProvider,
+    ErrorInterceptorProvider,
+    AuthService,
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,12 +1,17 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular/umd/components/alert/alert-controller";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler):Observable<HttpEvent<any>>{
-        console.log('Passou pelo Interceptor');
+        
         return next.handle(req)
           .catch((error, caught) => {
 
@@ -20,9 +25,55 @@ export class ErrorInterceptor implements HttpInterceptor{
 
             console.log('Erro detectado pelo interceptor');
             console.log(errorObj);
+
+            switch(errorObj.status){
+                case 401:
+                this.handle401();
+                break;
+
+                case 403:
+                this.handle403();
+                break;
+
+                default:
+                this.handleDefaultError(errorObj);
+            }
+
             return Observable.throw(errorObj);
           }) as any;
 
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle403(){
+      this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.stats + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
