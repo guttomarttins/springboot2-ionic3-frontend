660d3c1719ea7ec1407bba4b0e6c0c25695d841e
Carlos Martins <guttomarttins@gmail.com>
2018-07-22 12:33:57 -0300

    carregando produtos por categoria

---
+++ src/services/domain/produto.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+import { EstadoDTO } from "../../models/estado.dto";
+
+@Injectable()
+export class ProdutoService{
+   
+    constructor(public http: HttpClient){        
+    }
+
+    findByCategoria(categoria_id : String){
+        return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+}
