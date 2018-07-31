b23a37a50ec20fa2d2e4a3f06a53dc5ca6e7edb3
Carlos Martins <guttomarttins@gmail.com>
2018-07-31 20:12:08 -0300

    Criando pagina de escolha de endereco

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
-import { EstadoDTO } from "../../models/estado.dto";
 import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
